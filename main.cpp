ddddd02331d95b9d66ce51d4e2a03dc9885f0349
jorbDehmel <jdehmel@outlook.com>
2022-12-16 16:30:31 -0700

    Added demo

--- main.cpp
+++ main.cpp
@@ -2,24 +2,39 @@
 
 #include <iostream>
 #include <cmath>
+#include <set>
+
+double t = 0;
+
+double xOffset = 0;
+double yOffset = 0;
 
 bool a(double &x, double &y)
 {
-    y = x;
-    x += .01;
+    y = 5 * sin(t) + yOffset;
+    x = 5 * cos(t) + xOffset;
 
-    return true;
+    t += .001;
+
+    if (t > 7)
+    {
+        t = 0;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 int main()
 {
+    set<SDL_Keycode> keys;
     BarGraph g;
 
     g.equations.push_back(a);
 
     g.colors.push_back(Pixel(255, 0, 0, 255));
-    // g.colors.push_back(Pixel(0, 255, 0, 255));
-    // g.colors.push_back(Pixel(0, 0, 255, 255));
 
     SDL_Event event;
     bool isRunning = true;
@@ -34,13 +49,35 @@ int main()
             case SDL_KEYDOWN:
                 if (event.key.keysym.sym == 27)
                     isRunning = false;
+                keys.insert(event.key.keysym.sym);
+                cout << event.key.keysym.sym << '\n';
                 break;
             default:
+                keys.erase(event.key.keysym.sym);
                 break;
             }
         }
 
-        SDL_Delay(100);
+        if (keys.count(1073741906) != 0)
+        {
+            // up
+            yOffset += 1;
+        }
+        else if (keys.count(1073741904) != 0)
+        {
+            // left
+            xOffset -= 1;
+        }
+        else if (keys.count(1073741905) != 0)
+        {
+            // down
+            yOffset -= 1;
+        }
+        else if (keys.count(1073741903) != 0)
+        {
+            // right
+            xOffset += 1;
+        }
     }
 
     return 0;
